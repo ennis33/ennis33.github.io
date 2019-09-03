c8fc514e6a2d3f52a26ab5896f3f80a7350d8121
ennis33 <47521963+ennis33@users.noreply.github.com>
2019-09-03 16:37:28 +0100

    Add files via upload
    Initial commit

---
+++ contactform.php
@@ -0,0 +1,16 @@
+<?php
+if (isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $mailFrom = $_POST['mail'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+    
+    $mailTo ="ennisogidan@hotmail.co.uk"
+    $headers = "From: ".mailFrom;
+    $txt = $name. "has sent you a new email from your website .\n\n".$message; 
+    mail(
+        $mailTo, $subject, $txt, $headers;
+        header("Location: index.php?mailsend");
+    );
+}
+?>
