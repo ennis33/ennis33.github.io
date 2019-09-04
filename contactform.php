df6b09709bc5986df07b80e641515967bb16a354
ennis33 <47521963+ennis33@users.noreply.github.com>
2019-09-04 16:45:58 +0100

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -4,10 +4,10 @@ if (isset($_POST['submit'])){
     $mailFrom = $_POST['mail'];
     $subject = $_POST['subject'];
     $message = $_POST['message'];
-    
+}
     $mailTo ="ennisogidan@hotmail.co.uk"
     $headers = "From: ".mailFrom;
     $txt = $name. "has sent you a new email from your website .\n\n".$message; 
     mail($mailTo, $subject, $txt, $headers;);
     // header("Location: index.php?mailsend");
-}
+
